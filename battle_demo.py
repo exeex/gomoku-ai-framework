659754974dab0890af9bc8bc06a7e2445973307f
exeex <xray0h@gmail.com>
2018-10-01 15:07:25 +0800

    change file name

--- battle_demo.py
+++ battle_demo.py
@@ -1,8 +1,8 @@
 from goboard import GomokuBattleHandler
-from goboard.player import StupidAi, Human
+from goboard.player import StupidAi, StupidAi
 from goboard.judge import Win, Lose, Tie
-with GomokuBattleHandler(black_player=Human,
-                         white_player=Human,
+with GomokuBattleHandler(black_player=StupidAi,
+                         white_player=StupidAi,
                          board_size=(13, 13),
                          battle_file="lastest_battle.json",
                          use_gui=True) \
