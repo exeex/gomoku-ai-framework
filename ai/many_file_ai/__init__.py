58e7a13cb28e6777deee30494a83c6318c47a9dd
cswu <xray0h@gmail.com>
2018-10-24 11:07:24 +0800

    change board size

---
+++ ai/many_file_ai/__init__.py
@@ -0,0 +1,17 @@
+from .easy_ai import Ai
+
+"""
+If you don't add this line in __init__.py, you must import the Ai in the way:
+
+from ai.many_file_ai.easy_ai import Ai
+
+This will not be accepted in the Ai competition.
+
+
+this line would packaging the Ai class to be imported in this way:
+
+from ai.many_file_ai import Ai
+
+This one is acceptable.
+
+"""
